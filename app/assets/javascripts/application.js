2613073211995cc03a4ee43f23de86c22025d3fa
Sanjeev Nayak <snayak00+ofrgit@gmail.com>
2017-04-27 16:18:37 -0400

    betterized

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,37 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require angular/angular
 //= require angular-ui-router/release/angular-ui-router
-//= require main
-//= require_tree ./
+//= require_self
+//= require_tree .
+
+angular
+  .module('myApp', ['ui.router'])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $stateProvider
+      .state('notesIndex', {
+        url: '/notes',
+        component: 'notesIndex'
+      })
+      .state('notesShow', {
+        url: '/notes/:id',
+        component: 'notesShow'
+      })
+      .state('notesNew', {
+        url: '/notes/new',
+        component: 'notesNew'
+      })
+      .state('notesEdit', {
+        url: '/notes/:id/edit',
+        component: 'notesEdit'
+      });
+
+    // default fall back route
+    $urlRouterProvider.otherwise('/notes');
+
+    // enable HTML5 Mode for SEO
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+  });
